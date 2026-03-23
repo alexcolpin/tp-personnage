0f05bbdbda9362927fce1f5b40fb5309b24d0a16
alexcolpin <alex.colpin@edouardgand.fr>
2026-03-23 16:36:47 +0100

    partie1_finish

--- Program.cs
+++ Program.cs
@@ -1,9 +1,37 @@
 ﻿namespace tp_personnages;
 
+using System;
+
+class Personnage
+{
+    protected string nom;
+    protected int pointsDeVie;
+
+    public Personnage(string nom, int pointsDeVie)
+    {
+        this.nom = nom;
+        this.pointsDeVie = pointsDeVie;
+    }
+
+    public void RecevoirDegats(int degats)
+    {
+        pointsDeVie -= degats;
+    }
+
+    public void Afficher()
+    {
+        Console.WriteLine("Nom : " + nom + ", PV : " + pointsDeVie);
+    }
+}
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Personnage p = new Personnage("Jean", 100);
+        p.Afficher();
+
+        p.RecevoirDegats(20);
+        p.Afficher();
     }
 }

c4a00c5cc6a1caf390300a5d707e4706c936a15e
alexcolpin <alex.colpin@edouardgand.fr>
2026-03-25 13:46:59 +0100

    partie10_finish

--- Program.cs
+++ Program.cs
@@ -90,5 +90,10 @@ class Program
                 m.LancerSort();
             }
         }
+
+        if (personnages[1] is Guerrier)
+        {
+            Guerrier g = (Guerrier)personnages[1];
+        }
     }
 }
